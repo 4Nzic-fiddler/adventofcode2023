d9c6cb888c293304b4886025084c323a52faf02d
Randy Pargman <rpargman@proofpoint.com>
2023-12-07 22:39:12 -0800

    Solved day 7 part 2

--- day/7/src/main.zig
+++ day/7/src/main.zig
@@ -5,6 +5,7 @@ const expect = std.testing.expect;
 const Game = extern struct {
     hand: [5]u8,
     bid: u32,
+    jokers_not_jacks: bool,
     hand_type: enum(c_int) {
         HighCard=0,
         Pair=1,
@@ -15,42 +16,74 @@ const Game = extern struct {
         FiveOfAKind=6,
     },
 
-    pub fn init(hand: []const u8, bid: u32) Game {
+    pub fn init(hand: []const u8, bid: u32, jokers:bool) Game {
         var self: Game = .{
             .hand = [_]u8{0,0,0,0,0},
             .bid = bid,
+            .jokers_not_jacks = jokers,
             .hand_type = undefined,
         };
         // Copy VALUES, not pointer to array!
-        for (0..5) |i| {
-            self.hand[i] = switch (hand[i]) {
-                'T' => 9,
-                'J' => 10,
-                'Q' => 11,
-                'K' => 12,
-                'A' => 13,
-                '1'...'9' => hand[i] - '0' - 1,
-                else => 0,
-            };
+        if (jokers) {
+            for (0..5) |i| {
+                self.hand[i] = switch (hand[i]) {
+                    'T' => 10,
+                    'J' => 0,
+                    'Q' => 11,
+                    'K' => 12,
+                    'A' => 13,
+                    '1'...'9' => hand[i] - '0',
+                    else => 0,
+                };
+            }
+        } else {
+            for (0..5) |i| {
+                self.hand[i] = switch (hand[i]) {
+                    'T' => 9,
+                    'J' => 10,
+                    'Q' => 11,
+                    'K' => 12,
+                    'A' => 13,
+                    '1'...'9' => hand[i] - '0' - 1,
+                    else => 0,
+                };
+            }
         }
+        
         self.setHandType();
         return self;
     }
 
     pub fn printHand(self:Game) void {
         var c:u8 = undefined;
-        for (0..5) |i| {
-            c = switch (self.hand[i]) {
-                9 => 'T',
-                10 => 'J',
-                11 => 'Q',
-                12 => 'K',
-                13 => 'A',
-                0...8 => self.hand[i] + '0' + 1,
-                else => '?',
-            };
-            std.debug.print("{c}", .{c});
+        if (self.jokers_not_jacks) {
+            for (0..5) |i| {
+                c = switch (self.hand[i]) {
+                    0 => 'J', // Joker
+                    10 => 'T',
+                    11 => 'Q',
+                    12 => 'K',
+                    13 => 'A',
+                    1...9 => self.hand[i] + '0',
+                    else => '?',
+                };
+                std.debug.print("{c}", .{c});
+            }
+        } else {
+            for (0..5) |i| {
+                c = switch (self.hand[i]) {
+                    9 => 'T',
+                    10 => 'J', // Jack
+                    11 => 'Q',
+                    12 => 'K',
+                    13 => 'A',
+                    0...8 => self.hand[i] + '0' + 1,
+                    else => '?',
+                };
+                std.debug.print("{c}", .{c});
+            }
         }
+        
         std.debug.print("\n", .{});
     }
 
@@ -81,11 +114,41 @@ const Game = extern struct {
         var three_count:u8 = 0;
         var four_count:u8 = 0;
         var five_count:u8 = 0;
-        for (0..14) |i| {
+        // If J means Joker instead of Jack, then we will NOT count card 0 (Joker) as a pair
+        var index_of_one:u8 = if (self.jokers_not_jacks) 1 else 0;
+        for (index_of_one..14) |i| {
             if (counts[i]==2) {pair_count += 1;}
             else if (counts[i]==3) {three_count += 1;}
             else if (counts[i]==4) {four_count += 1;}
             else if (counts[i]==5) {five_count += 1;}
+        }
+        // Make Jokers increase pairs to threes, threes to fours, etc.
+        if (self.jokers_not_jacks) {
+            // Iterate through the jokers, increasing hand value each time
+            for (0..counts[0]) |x| {
+                _ = x; // unused
+                // Go in order from best to worst hand
+                if (four_count > 0) {
+                    // If we had any four of a kind before, make it five of a kind
+                    four_count -= 1;
+                    five_count += 1;
+                } else if (three_count > 0) {
+                    // Three of a kind becomes four of a kind
+                    three_count -= 1;
+                    four_count += 1;
+                } else if (pair_count == 2) {
+                    // Two pair becomes full house
+                    pair_count -= 1;
+                    three_count += 1;
+                } else if (pair_count > 0) {
+                    // Pair becomes three of a kind
+                    pair_count -= 1;
+                    three_count += 1;
+                } else {
+                    // No pairs, so make it a pair
+                    pair_count += 1;
+                }
+            }
         }
 
         if (five_count == 1) {
@@ -135,7 +198,9 @@ test "custom sort for arraylist of Game" {
     try expect(list.items[3].bid == 4);
 }
 
-pub fn solvePartOne() !u64 {
+pub fn solvePart(part_num:u8) !u64 {
+    // In part two, J means Joker not Jack
+    const jokers_not_jacks:bool = (part_num == 2);
     var result:u32 = 0;
     const allocator = std.heap.page_allocator;
     var games: std.ArrayList(Game) = std.ArrayList(Game).init(allocator);
@@ -148,7 +213,6 @@ pub fn solvePartOne() !u64 {
     // Create a 4k buffer to read lines into
     var buf: [4096]u8 = undefined;
 
-
     // Read lines from stdin until EOF is reached. Lines are delimited by '\n'
     while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
         var trimmed_line = std.mem.trim(u8, line, " ");
@@ -157,7 +221,7 @@ pub fn solvePartOne() !u64 {
             if (token_iterator.next()) |hand_str| {
                 if (token_iterator.next()) |bid_str| {
                     var bid:u32 = try std.fmt.parseInt(u32, bid_str, 10);
-                    try games.append(Game.init(hand_str, bid));
+                    try games.append(Game.init(hand_str, bid, jokers_not_jacks));
                 }
             }
         }
@@ -178,6 +242,9 @@ pub fn solvePartOne() !u64 {
 }
 
 pub fn main() !void {
-    const answer_one = try solvePartOne();
+    const answer_one = try solvePart(1);
     std.debug.print("Answer Part One: {d}\n", .{answer_one});
+
+    const answer_two = try solvePart(2);
+    std.debug.print("Answer Part Two: {d}\n", .{answer_two});
 }
