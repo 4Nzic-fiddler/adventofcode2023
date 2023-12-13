34ea1ee945d60a2657095689ac9f52d009857b24
Randy Pargman <rpargman@proofpoint.com>
2023-12-13 14:47:49 -0800

    Day 5 part 2

--- day/5/src/main.zig
+++ day/5/src/main.zig
@@ -15,11 +15,22 @@ const Conversion = struct{
         return source >= self.source_start and source < self.source_start + self.length;
     }
 
+    pub fn destInRange(self:Conversion, dest:u64) bool {
+        return dest >= self.dest_start and dest < self.dest_start + self.length;
+    }
+
     pub fn convert(self: Conversion, source: u64) u64 {
         if (!self.inRange(source)) {
             return source;
         }
         return (source - self.source_start) + self.dest_start;
+    }
+
+    pub fn reverseConvert(self: Conversion, dest: u64) u64 {
+        if (!self.destInRange(dest)) {
+            return dest;
+        }
+        return (dest - self.dest_start) + self.source_start;
     }
 };
 test "conversion test" {
@@ -33,6 +44,30 @@ test "conversion test" {
     try expect(c1.convert(5) == 105);
     try expect(c1.convert(20) == 20);
 }
+
+// Custom sort function for ArrayList(Conversion) 
+fn cmpByDestStart(context: void, a: Conversion, b: Conversion) bool {
+    _ = context;
+    return a.dest_start < b.dest_start;
+}
+test "custom sort for arraylist of Conversion" {
+    const allocator = std.testing.allocator;
+    var list = ArrayList(Conversion).init(allocator);
+    defer list.deinit();
+    try list.append(Conversion{ .dest_start = 50, .source_start = 98, .length = 2 });
+    try list.append(Conversion{ .dest_start = 52, .source_start = 50, .length = 48 });
+    try list.append(Conversion{ .dest_start = 100, .source_start = 0, .length = 10 });
+    try list.append(Conversion{ .dest_start = 0, .source_start = 0, .length = 10 });
+
+    std.mem.sort(Conversion, list.items, {}, cmpByDestStart);
+    for (list.items) |conversion| {
+        std.debug.print("dest_start: {d}, source_start: {d}, length: {d}\n", .{conversion.dest_start, conversion.source_start, conversion.length});
+    }
+    try expect(list.items[0].dest_start == 0);
+    try expect(list.items[1].dest_start == 50);
+    try expect(list.items[2].dest_start == 52);
+}
+
 
 // ConversionSet holds all the conversions for a given source and destination
 const ConversionSet = struct{
@@ -60,6 +95,16 @@ const ConversionSet = struct{
                 
         }
         return source; // if no explicit conversion, return the source
+    }
+
+    // Use this to convert a destination number back to a source number (for part 2)
+    pub fn reverseConvert(self: ConversionSet, dest: u64) u64 {
+        for (self.conversions_ptr.*.items) |conversion| {
+            if (conversion.destInRange(dest)) {
+                return conversion.reverseConvert(dest);
+            }   
+        }
+        return dest; // if no explicit conversion, return the dest
     }
    
 };
@@ -179,9 +224,9 @@ test "parseConversion test" {
 pub fn convertFromTo(source: u64, source_name: []const u8, dest_name: []const u8, conversion_hashmap:std.StringHashMap(ConversionSet)) u64 {
     var conversion_set = conversion_hashmap.get(source_name); // get the first conversion set from the source type to whatever
     if (conversion_set) |cs| {
-        std.debug.print("Converting {d} from {s} to {s}\n", .{source, cs.source_name.slice(), cs.dest_name.slice()});
+        //std.debug.print("Converting {d} from {s} to {s}\n", .{source, cs.source_name.slice(), cs.dest_name.slice()});
         if (std.mem.eql(u8, cs.dest_name.slice(), dest_name)) {
-            std.debug.print("Found final conversion to {s}\n", .{dest_name});
+            //std.debug.print("Found final conversion to {s}\n", .{dest_name});
             return cs.convert(source);
         } else {
             return convertFromTo(cs.convert(source), cs.dest_name.slice(), dest_name, conversion_hashmap);
@@ -191,7 +236,7 @@ pub fn convertFromTo(source: u64, source_name: []const u8, dest_name: []const u8
 }
 
 
-pub fn solvePartOne() !u64 {
+pub fn solvePart(part_num:u8) !u64 {
     var lowest_location: u64 = std.math.maxInt(u64);
     const allocator = std.heap.page_allocator;
     // Hashmap indexed by source name, containing a ConversionSet
@@ -255,8 +300,8 @@ pub fn solvePartOne() !u64 {
                     if (!result) {
                         std.debug.print("Failed to parse conversion line: {s}\n", .{line});
                     } else {
-                        std.debug.print("Parsed conversion line: {s}\n", .{line});
-                        std.debug.print("Conversion array at index {d} has length: {d}\n", .{conversion_array.items.len-1, conversion_array.items[conversion_array.items.len - 1].items.len});
+                        //std.debug.print("Parsed conversion line: {s}\n", .{line});
+                        //std.debug.print("Conversion array at index {d} has length: {d}\n", .{conversion_array.items.len-1, conversion_array.items[conversion_array.items.len - 1].items.len});
                     }
                 }
             }
@@ -264,18 +309,88 @@ pub fn solvePartOne() !u64 {
     }
 
     // Finally all set up, now go through seeds and find the lowest location
+
+    // Part 2 solution (the faster way): Sort all the conversion arrays by dest_start
+    // and work backwards from the location conversion set to find the lowest range of
+    // locations, then go up through humidity, temperature, light, water, fertilizer, soil, seed
+    var prev_seed: u64 = 0;
+    var seed_num: usize = 0;
+    // Create an ArrayList of seed ranges as Conversions so we can quickly check them
+    var seed_ranges = ArrayList(Conversion).init(allocator);
+    defer seed_ranges.deinit();
+    // the seed range is only needed for part 2 but it is fast to create
+    for (seeds_array.items) |seed| {
+        if (seed_num % part_num == 1) { // when part_num==1, this is never true. When part_num==2, this is true every other time
+            var seed_range = Conversion{ .dest_start = prev_seed, .source_start = prev_seed, .length = seed };
+            try seed_ranges.append(seed_range);
+        }
+    }
+    const reverse_order:[5][]const u8 = [_][]const u8{"temperature", "light", "water", "fertilizer", "soil"};
+    var humidity_to_location = conversion_hashmap.get("humidity");
+    if (part_num == 2 and humidity_to_location) |h2l| {
+        // Sort the humidity-to-location conversion array by dest_start, low to high
+        // That gives us the lowest possible location outputs first so we can work backwards efficiently
+        std.mem.sort(Conversion, h2l.conversions_ptr.*.items, {}, cmpByDestStart);
+        for (h2l.conversions_ptr.*.items) |conversion| {
+            // start with the lowest location destination range and work backwards to find any seed ranges that fall 
+            // in the range to produce the lowest location in that chain
+            std.debug.print("Finding a seed that produces location range {d} to {d}...\n", .{conversion.dest_start, conversion.dest_start + conversion.length});
+            var low_value_to_get = conversion.source_start;
+            var high_value_to_get = conversion.source_start + conversion.length;
+            for (low_value_to_get..high_value_to_get) | value_to_get | {
+                var val = value_to_get; // need var not const here
+                for (reverse_order) |set_name| { //temperature, light, water, fertilizer, soil
+                    var conversion_set = conversion_hashmap.get(set_name);
+                    if (conversion_set) |cs| {
+                        val = cs.reverseConvert(val);
+                    }
+                }
+                // Now we've ended up with the seed number that is required to produce the lowest location
+                // Check to see if it is in any of the seed ranges
+                for (seed_ranges.items) |seed_range| {
+                    if (seed_range.inRange(val)) {
+                        std.debug.print("!! Found the seed that produces the lowest location: {d}\n", .{val});
+                        var location = convertFromTo(val, "seed", "location", conversion_hashmap);
+                        std.debug.print("Seed {d} produces location {d}\n", .{val, location});
+                        lowest_location = location;
+                        break;
+                    }
+                }
+            }   
+        }
+    }
+    
+    prev_seed = 0;
+    seed_num = 0;
     for (seeds_array.items) |seed| {
         var location = convertFromTo(seed, "seed", "location", conversion_hashmap);
+        // This is all we need to do differently for part 2, the straightforward but VERY slow way
+        // if (seed_num % part_num == 1) { // when part_num==1, this is never true. When part_num==2, this is true every other time
+        //     std.debug.print("Checking seed range: {d} to {d}, prev_seed: {d}\n", .{prev_seed, prev_seed+seed});
+        //     for (prev_seed..prev_seed+seed) |i| {
+        //         location = convertFromTo(i, "seed", "location", conversion_hashmap);
+        //     }
+        // }
+        // End of part two difference
+        // Note: this is unacceptably slow. 
+        // Better plan: work backwards from the lowest location conversion line:
+        // 0 262282387 23738616
+        
         if (location < lowest_location) {
             lowest_location = location;
         }
+        prev_seed = seed;
+        seed_num += 1;
     }
 
     return lowest_location;
 }
 
 pub fn main() !void {
-    var lowest_location = try solvePartOne();
+    var lowest_location = try solvePart(1);
     std.debug.print("Part 1 (lowest location): {d}\n", .{lowest_location});
+
+    lowest_location = try solvePart(2);
+    std.debug.print("Part 2 (lowest location): {d}\n", .{lowest_location});
 }
 
